2ac3481e77dbfab1c7cc0a953e9f9f77e3b6d50e
xFA25E <55886566+xFA25E@users.noreply.github.com>
2020-11-25 18:27:00 +0100

    implemented mediainfo-mode with file-handlers
    So it would not hang emacs on big files

--- mediainfo-mode.el
+++ mediainfo-mode.el
@@ -22,40 +22,84 @@
 
 ;; Show mediainfo data when opening media file in Emacs
 ;;
-;; Call `TORRENT-MODE-SETUP'.
+;; Call `MEDIAINFO-MODE-SETUP'.
 
 ;;; Code:
+
+
+;;;; REQUIRES
+
+(require 'rx)
+
+
+;;;; VARIABLES
 
 (defvar mediainfo-mode-command "mediainfo %s"
   "The shell command to use for `mediainfo-mode'.")
 
-(defun mediainfo-mode-revert-buffer ()
-  (interactive)
-  (when (eq 'mediainfo-mode major-mode)
-    (let ((inhibit-read-only t))
-      (erase-buffer)
-      (insert
-       (shell-command-to-string
-        (format mediainfo-mode-command
-                (shell-quote-argument (buffer-file-name)))))
-      (set-buffer-modified-p nil))))
+(defvar mediainfo-mode-file-regexp
+  (rx "."
+      (or "flac" "m4a" "mp3" "ogg" "opus" "webm" "mkv" "mp4" "avi" "mpg" "mov"
+          "3gp" "vob" "wmv" "aiff" "wav")
+      eos)
+  "A regexp used to distinguish mediainfo-supported files.")
+
+
+;;;; FUNCTIONS
+
+(defun mediainfo-mode--run-command (filename)
+  "Run medainfo command on `FILENAME' and return a result string."
+  (shell-command-to-string
+   (format mediainfo-mode-command (shell-quote-argument filename))))
+
+(defun mediainfo-mode--file-handler (operation &rest args)
+  "A special file handler for mediainfo.
+Apply `INSERT-FILE-CONTENTS' `OPERATION' on `ARGS'."
+  (cl-case operation
+    (insert-file-contents
+     (cl-destructuring-bind (filename &optional visit beg end replace) args
+       (when (or beg end)
+         (error "Beginning and end does not make sense for mediainfo"))
+       (when (and visit (/= 0 (buffer-size)) (not replace))
+         (error "Cannot visit non-empty buffer"))
+       (let* ((filename (expand-file-name filename))
+              (mediainfo-string (mediainfo-mode--run-command filename)))
+         (if replace
+             (let ((inhibit-read-only t))
+               (erase-buffer)
+               (insert mediainfo-string))
+           (insert mediainfo-string))
+         (when visit
+           (set-visited-file-name filename)
+           (set-visited-file-modtime (current-time)))
+         (list filename (length mediainfo-string)))))
+    (t
+     (let ((inhibit-file-name-handlers
+            (cons 'mediainfo-mode--file-handler
+                  (and (eq inhibit-file-name-operation operation)
+                       inhibit-file-name-handlers)))
+           (inhibit-file-name-operation operation))
+       (apply operation args)))))
+
+
+;;;; COMMANDS
 
 ;;;###autoload
 (define-derived-mode mediainfo-mode special-mode "Mediainfo"
-  (mediainfo-mode-revert-buffer)
   (read-only-mode))
 
 ;;;###autoload
 (defun mediainfo-mode-setup ()
   "Make `mediainfo-mode' get called automatically for mediainfo files."
+  (interactive)
+
+  (add-to-list
+   'file-name-handler-alist
+   (cons mediainfo-mode-file-regexp 'mediainfo-mode--file-handler))
+
   (add-to-list
    'auto-mode-alist
-   (cons
-    (rx "."
-        (or "flac" "m4a" "mp3" "ogg" "opus" "webm" "mkv" "mp4" "avi" "mpg" "mov"
-            "3gp" "vob" "wmv" "aiff" "wav")
-        eos)
-    'mediainfo-mode)))
+   (cons mediainfo-mode-file-regexp 'mediainfo-mode)))
 
 (provide 'mediainfo-mode)
 ;;; mediainfo-mode.el ends here
